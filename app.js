fa8429966f8c6af51119950693ee961bd2011244
cadupuy <charlesantoine.dupuy@gmail.com>
2022-03-06 18:31:45 +0100

    wip

--- app.js
+++ app.js
@@ -18,6 +18,7 @@ app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(methodOverride());
+app.use(express.static(path.join(__dirname, 'public')));
 
 const Prismic = require('@prismicio/client');
 const PrismicH = require('@prismicio/helpers');
